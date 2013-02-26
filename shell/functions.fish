3f4fe8231009b598d29ba675f916f26ff587081b
Jussi Kalliokoski <jussi.kalliokoski@gmail.com>
2013-02-26 17:41:31 +0200

    Added some git utilities

--- shell/functions.fish
+++ shell/functions.fish
@@ -30,6 +30,22 @@ function gs
 	git status $argv
 end
 
+# Lists all files in the git tree
+function git-files
+	git ls-tree -r master | awk "{print \$4}"
+end
+
+# Performs a sed operation for all files in the git tree
 function git-sed
-	sed $argv (git ls-tree -r master | awk "{print \$4}")
+	sed $argv (git-files)
+end
+
+# Prints the ID of commit n, where n=1 is the latest commit
+function git-cid
+	git log --color=never -n $argv[1] | grep "^commit" | awk "{ print \$2 }" | tail -n 1
+end
+
+# Compares (diff) two commits.
+function git-cc
+	git diff (git-cid $argv[1])..(git-cid $argv[2])
 end
