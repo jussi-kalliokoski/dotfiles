246c7ed54cc253825bf689a649bcf31906b86c2c
Jussi Kalliokoski <jussi.kalliokoski@gmail.com>
2014-03-24 09:42:01 +0200

    Unified indentation in fish configs.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,35 +4,35 @@ set -x EDITOR "vim"
 set -x BROWSER "open"
 
 function git_info -d "Print a star if the current git branch is dirty"
-	git diff --quiet HEAD ^&-
-	if test $status = 1
-		echo '*'
-	end
+    git diff --quiet HEAD ^&-
+    if test $status = 1
+        echo '*'
+    end
 end
 
 function git_branch -d "Print the current git branch"
-	git symbolic-ref --short HEAD 2>/dev/null
+    git symbolic-ref --short HEAD 2>/dev/null
 end
 
 # Simplified: [DIRECTORY] - [USERMODE] - <GITSTATUS>
 function fish_prompt
-	set_color --bold blue
-	echo -n '['
-	set_color --bold white
-	echo -n (prompt_pwd)
-	set_color --bold blue
-	echo -n '] - ['
-	set_color --bold purple
-	echo -n '$'
-	set_color --bold blue
-	echo -n '] <'
-	echo -n (git_branch)
-	echo -n (git_info)
-	set_color --bold blue
-	echo -n '> '
-	set_color normal
-	# Tell TMUX the CWD
-	if set -q TMUX; tmux setenv TMUXPWD_(tmux display -p "#D" | tr -d "%") "$PWD"; end
+    set_color --bold blue
+    echo -n '['
+    set_color --bold white
+    echo -n (prompt_pwd)
+    set_color --bold blue
+    echo -n '] - ['
+    set_color --bold purple
+    echo -n '$'
+    set_color --bold blue
+    echo -n '] <'
+    echo -n (git_branch)
+    echo -n (git_info)
+    set_color --bold blue
+    echo -n '> '
+    set_color normal
+    # Tell TMUX the CWD
+    if set -q TMUX; tmux setenv TMUXPWD_(tmux display -p "#D" | tr -d "%") "$PWD"; end
 end
 
 # Load aliases and other functions
--- .config/fish/functions.fish
+++ .config/fish/functions.fish
@@ -1,14 +1,14 @@
 function fish_reload
-	. ~/.config/fish/config.fish
+    . ~/.config/fish/config.fish
 end
 
 function prompt
-	echo $argv "(y\N)"
-	read -p "" answer
-	if test $answer = "y"
-		return 0
-	end
-	return 1
+    echo $argv "(y\N)"
+    read -p "" answer
+    if test $answer = "y"
+        return 0
+    end
+    return 1
 end
 
 function print_array
@@ -18,34 +18,34 @@ function print_array
 end
 
 function dotfiles_sync
-	set old_pwd (pwd)
-	cd ~/.dotfiles
+    set old_pwd (pwd)
+    cd ~/.dotfiles
 
-	git remote rm origin
-	git remote add origin git@github.com:jussi-kalliokoski/dotfiles.git
+    git remote rm origin
+    git remote add origin git@github.com:jussi-kalliokoski/dotfiles.git
 
-	if contains force $argv
-		git reset --hard origin/master
-		cd $old_pwd
-		return 0
-	end
+    if contains force $argv
+        git reset --hard origin/master
+        cd $old_pwd
+        return 0
+    end
 
-	if not git diff --quiet HEAD ^&-
-		echo "You have uncommitted changes to your dotfiles."
-		if prompt "Would you like to commit them in?"; and git commit -a
+    if not git diff --quiet HEAD ^&-
+        echo "You have uncommitted changes to your dotfiles."
+        if prompt "Would you like to commit them in?"; and git commit -a
 
-			git pull --rebase -u origin master
-			git push -u origin master
-			cd $old_pwd
-			return 0
-		else
-			echo "Please commit your local changes before sync."
-			cd $old_pwd
-			return 1
-		end
-	end
-	git pull -u origin master
-	cd $old_pwd
+            git pull --rebase -u origin master
+            git push -u origin master
+            cd $old_pwd
+            return 0
+        else
+            echo "Please commit your local changes before sync."
+            cd $old_pwd
+            return 1
+        end
+    end
+    git pull -u origin master
+    cd $old_pwd
 end
 
 function now -d "Prints the current time (UNIX Epoch) in milliseconds"
@@ -53,43 +53,43 @@ function now -d "Prints the current time (UNIX Epoch) in milliseconds"
 end
 
 function tn
-	tmux new-session -s $argv
+    tmux new-session -s $argv
 end
 
 function ta
-	tmux attach -t $argv
+    tmux attach -t $argv
 end
 
 function tls
-	tmux ls
+    tmux ls
 end
 
 function gg
-	git grep $argv
+    git grep $argv
 end
 
 function gb
-	git branch $argv
+    git branch $argv
 end
 
 function gc
-	git checkout $argv
+    git checkout $argv
 end
 
 function gs
-	git status $argv
+    git status $argv
 end
 
 function gw
-	git whatchanged $argv
+    git whatchanged $argv
 end
 
 function gls
-	git ls-files $argv
+    git ls-files $argv
 end
 
 function gfa
-	git fetch --all
+    git fetch --all
 end
 
 function gpb
@@ -97,7 +97,7 @@ function gpb
 end
 
 function add-host
-	printf "%s\t%s" (host $argv[1] | awk "{print \$4}") $argv[1]
+    printf "%s\t%s" (host $argv[1] | awk "{print \$4}") $argv[1]
 end
 
 function git-list-branches -d "Lists branches in the git repo without highlighting current one"
@@ -105,48 +105,48 @@ function git-list-branches -d "Lists branches in the git repo without highlighti
 end
 
 function git-pick-file -d "Cherry pick commits that have changes to specified files"
-	# store current branch
-	set -l branch (git_branch)
-	# checkout target branch
-	git checkout $argv[1]
-	# shift the argv array
-	set -e argv[1]
-	# store the list of commits containing changes to the specified files
-	set -l commits (git log --pretty="%H" --reverse -- $argv)
-	# checkout back to the original branch
-	git checkout $branch
-	# cherry-pick each stored commit
-	for commit in $commits
-		git cherry-pick $commit
-	end
+    # store current branch
+    set -l branch (git_branch)
+    # checkout target branch
+    git checkout $argv[1]
+    # shift the argv array
+    set -e argv[1]
+    # store the list of commits containing changes to the specified files
+    set -l commits (git log --pretty="%H" --reverse -- $argv)
+    # checkout back to the original branch
+    git checkout $branch
+    # cherry-pick each stored commit
+    for commit in $commits
+        git cherry-pick $commit
+    end
 end
 
 function git-sed -d "Performs a sed operation for all files in the git tree"
-	sed $argv (gls)
+    sed $argv (gls)
 end
 
 function git-get-commits -d "Gets the hashes of commits in the log."
-	git log --pretty="%H" $argv
+    git log --pretty="%H" $argv
 end
 
 function git-get-commit-index -d "Gets the how many commits ago a commit in the log is."
-	set -l commits (git-get-commits)
-	set -l index 1
-	for commit in $commits
-		if echo $commit | ack $argv[1] > /dev/null
-			break
-		end
-		set index (math $index + 1)
-	end
-	echo $index
+    set -l commits (git-get-commits)
+    set -l index 1
+    for commit in $commits
+        if echo $commit | ack $argv[1] > /dev/null
+            break
+        end
+        set index (math $index + 1)
+    end
+    echo $index
 end
 
 function git-cid -d "Print the ID of the commit n, where n=1 is the latest commit"
-	git-get-commits -n $argv[1] | tail -n 1
+    git-get-commits -n $argv[1] | tail -n 1
 end
 
 function git-cc -d "Compare (diff) two commits"
-	git diff (git-cid $argv[2])..(git-cid $argv[1])
+    git diff (git-cid $argv[2])..(git-cid $argv[1])
 end
 
 function tunnel -d "Tunnel a local port to a remote port"
